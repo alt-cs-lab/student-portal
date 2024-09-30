002d641c97028215dd5c8da932ee296b2c8f381a
Kalithar <89866543+Kalithar@users.noreply.github.com>
2024-09-30 15:03:37 +0000

    Working Vue component in components, Button.vue. Added it to App.vue

---
+++ vue-client/src/components/Button.jsx
@@ -0,0 +1,14 @@
+import "../styles/Button.scss";
+
+const ApplyButton = () => {
+  return (
+    <div class="button-container-1">
+      <span class="mas">Apply!</span>
+      <button type="button" name="Hover">
+        Apply!
+      </button>
+    </div>
+  )
+}
+
+export default {ApplyButton};
