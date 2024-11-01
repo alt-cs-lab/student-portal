4b03ac9aa8e1e0c0cd496c3d21e74f1c07d9e5ed
LukeCMoeller <111709091+LukeCMoeller@users.noreply.github.com>
2024-11-01 15:23:16 +0000

    test

--- vue-client/src/sub-apps/profile-app/ProfilesForm.vue
+++ vue-client/src/sub-apps/profile-app/ProfilesForm.vue
@@ -65,7 +65,7 @@
                 <div class="col-12" style="margin-top: 20px;">
                   <div style="background-color: gray; border: 3px solid #757575; border-radius: 10px;" class="col border-round-sm">
                     <div class="col-12 sml">
-                      <img :src="githubText" alt="github text" width="150" height="50" style="margin: 15px;" />
+                      <img :src="githubText" alt="github text" style="text-align: center; margin: 15px;  max-width: 70%; max-height: 50%;" />
                       <h4 :class="styles['text']" style="text-align: center; color:white">Click the button below to link your GitHub account.</h4>
                     </div>
                     <div class="col flex align-items-center justify-content-center">
